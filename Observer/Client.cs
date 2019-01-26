7f3cedb74da3b6baee021e6c0bac98a42f98334b
admin <admin@HP-HP>
2019-01-27 01:39:30 +0300

    Implemented Observer pattern

---
+++ Observer/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new ConcreteSubject();
+            Observer observer = new Observer((observerState) => Console.WriteLine(observerState + " 1"));
+            subject.Event += observer;
+            subject.Event += (observerState) => Console.WriteLine(observerState + " 2");
+            subject.State = "State ...";
+
+            subject.Notify();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Observer/Subject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public abstract class Subject
+    {
+        protected Observer observers = null;
+
+        public event Observer Event
+        {
+            add { observers += value; }
+            remove { observers -= value; }
+        }
+
+        public abstract string State
+        {
+            get; set;
+        }
+
+        public abstract void Notify();
+    }
+}
