68be08c851e0806a2b3c1470957c8b32a154c8c2
ruslan.abdraev@gmail.com <ruslan.abdraev@gmail.com>
2019-01-31 19:01:46 +0300

    Implemented TemplateMethod pattern.

---
+++ TemplateMethod/AbstractClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    public abstract class AbstractClass
+    {
+        public void TemplateMethod()
+        {
+            Console.WriteLine("TemplateMethod.AbstractClass.TemplateMethod method call.");
+
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+        }
+
+        protected abstract void PrimitiveOperation1();
+        protected abstract void PrimitiveOperation2();
+
+    }
+}
---
+++ TemplateMethod/ConcreteClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    public class ConcreteClass : AbstractClass
+    {
+        protected override void PrimitiveOperation1()
+        {
+            Console.WriteLine("TemplateMethod.ConcreteClass.PrimitiveOperation1 method call.");
+        }
+
+        protected override void PrimitiveOperation2()
+        {
+            Console.WriteLine("TemplateMethod.ConcreteClass.PrimitiveOperation2 method call.");
+        }
+    }
+}
